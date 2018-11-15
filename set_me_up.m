5f19a656b200fc8bed1a87909926d94f4028f30c
Liu-ko <33573204+Liu-ko@users.noreply.github.com>
2018-11-15 15:55:27 +0000

    Similarity sheck with either mean or histogram comparison

--- set_me_up.m
+++ set_me_up.m
@@ -3,6 +3,25 @@ function [A, hist, target, tar_hist] = set_me_up()
     A = filesRead("./Images/manmade_training/out_manmade_1k/");
     target = imread("./Images/mosaic_target1.jpg");
     
+    % sizes
+    pixX = 1000;
+    pixY = 1500;
+    blockX = 40;
+    blockY = 60;
+    
+    %block sizes
+    x = pixX/blockX;
+    y = pixY/blockY;
+    
+    %resize all the images
+    target = imresize(target, [pixX, pixY]);
+    [aa, num] = size(A);
+    a = cell(1, num);
+    for i = 1:num
+        a{i} = imresize(A{i}, [x, y]); 
+    end
+    
+    A = a
     %get all the histograms
     [x, num] = size(A);
     hist = cell(1, num);
@@ -11,5 +30,6 @@ function [A, hist, target, tar_hist] = set_me_up()
     end
     tar_hist = calculate_RGB_hist(target);
     
-    clc
+    clc;
+    
 end
--- similarity_check.m
+++ similarity_check.m
@@ -1,23 +1,12 @@
 function rez = similarity_check(A, T, hist)
-    T = imresize(T, [1000, 1500]);
-    rez = T;
-    
-    %block 40x60
+    [aa, num] = size(A);
+    %block sizes
     x = 25;
     y = 25;
-    
-    %resize all the source images
-    [aa, num] = size(A);
-    a = cell(1, num);
-    for i = 1:num
-        a{i} = imresize(A{i}, [x, y]);  
-        %a{i} = crop_image(A{i})
-    end
-    
     %get mean values
     means = cell(1, num);
     for z = 1:num
-        means{z} = mean_calculation(a{z});
+        means{z} = mean_calculation(A{z});
     end    
     
     aaa = zeros(1, num);
@@ -25,16 +14,21 @@ function rez = similarity_check(A, T, hist)
         for j = 1:60
             that_piece = T((i-1)*x+1:i*x, (j-1)*y+1:j*y, :);
             piece_mean = mean_calculation(that_piece);  
-            piece_hist = calculate_RGB_hist(that_piece);
+            %piece_hist = calculate_RGB_hist(that_piece);
             %find the most similar mean
             for z = 1:num
                 the_mean = means{z};
-                the_hist = hist{z};
+                %the_hist = hist{z};
                 aaa(z) = sqrt(sum((piece_mean - the_mean).^ 2));
-                %aaa(z) = sqrt(sum((piece_hist - the_hist).^ 2));
+                
+                %histogram shait
+                %e1 = sum((piece_hist(:,:,1) - the_hist(:,:,1)).^ 2);                
+                %e2 = sum((piece_hist(:,:,2) - the_hist(:,:,2)).^ 2);
+                %e3 = sum((piece_hist(:,:,3) - the_hist(:,:,3)).^ 2);
+                %aaa(z) = sqrt(e1+e2+e3);
             end    
             [val, index] = min(aaa);
-            rez((i-1)*x+1:i*x, (j-1)*y+1:j*y, :) = a{index};
+            rez((i-1)*x+1:i*x, (j-1)*y+1:j*y, :) = A{index};
         end
     end
 end
