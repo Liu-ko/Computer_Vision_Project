69d8b129fb624cb3cdbca955ac1ad324bea0d820
LiamRattray <ltrattray@dundee.ac.uk>
2018-11-15 14:13:42 +0000

    Added scripts
    Added scripts for each step of creating the mosaic.
    Edited crop function to return the image

--- crop_img.m
+++ crop_img.m
@@ -1,7 +1,7 @@
 %img = imread("D:\comp_vision\Computer_Vision_Project-master\images\sun_aabwvttncoffagty.jpg");
 
 %creating function with input image and parameters
-function crop_img(img, a, b, c, d)
+function y = crop_img(img, a, b, c, d)
     cropped_img = imcrop(img,[ a, b, c, d]);
     subplot(1,2,1)
     imshow(img)
@@ -9,5 +9,6 @@ function crop_img(img, a, b, c, d)
     subplot(1, 2, 2)
     imshow(cropped_img)
     title("cropped image")
+    y = cropped_img
 end
 
