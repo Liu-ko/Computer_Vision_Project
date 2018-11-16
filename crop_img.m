1292b8bc813be313e33062d53e8de6c15bd84dae
Eniko Kovacs <EKovacs@dundee.ac.uk>
2018-11-16 13:02:05 +0000

    cleaned cropping&resampling

--- crop_img.m
+++ crop_img.m
@@ -3,12 +3,7 @@
 %creating function with input image and parameters
 function y = crop_img(img, a, b, c, d)
     cropped_img = imcrop(img,[ a, b, c, d]);
-    subplot(1,2,1)
-    imshow(img)
-    title("original")
-    subplot(1, 2, 2)
-    imshow(cropped_img)
-    title("cropped image")
-    y = cropped_img
+   
+    y = cropped_img;
 end
 
--- resample.m
+++ resample.m
@@ -1,23 +1,28 @@
-function resample()
-i = imread("D:\comp_vision\Computer_Vision_Project-master\images\sun_aabwvttncoffagty.jpg");
+function r = resample()
+i = imread("D:\comp_vision\Computer_Vision_Project-master\images\out_manmade_1k\out_natural_1k\sun_adttjkmojmfrsozw.jpg");
 
     img = imgaussfilt(i);
     interpolant = griddedInterpolant(double(img))
     [sx, sy, sz] = size(img)
-    nx = (0:5/6:sx)';
-    ny = (0:5/6:sy)';
+    nx = (0:5/3:sx)';
+    ny = (0:5/3:sy)';
     nz = (1:sz)';
     nimg = uint8(interpolant({nx, ny, nz}));
-    figure
-    subplot(1, 2, 1);
-    imshow(img);
-    subplot(1, 2, 2);
-    imshow(nimg);
+   
+    %nimg = imadjust(nimg,[0.6, 0.7], [0.7, .95]);
+    
+    %nimg = crop_img(nimg,0, 10, 200, 190);
+    %figure
+    %subplot(1, 2, 1);
+    %imshow(img);
+    %subplot(1, 2, 2);
+    %imshow(nimg);
     
     totpxi = numel(i);
     totpximg = numel( nimg );
-    disp('input:')
-    disp(totpxi)
-    disp('output')
-    disp(totpximg)
+    %disp('input:');
+    %disp(totpxi);
+    %disp('output');
+    %disp(totpximg);
+    r = nimg;
 end
